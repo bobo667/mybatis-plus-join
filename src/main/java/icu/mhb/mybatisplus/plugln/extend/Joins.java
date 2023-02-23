75c74cbe677a7482edd6f056c23bd69746751db7
shiweizhi <shiweizhi@haier.com>
2023-02-23 10:40:38 +0800

    添加别名字段

--- src/main/java/icu/mhb/mybatisplus/plugln/extend/Joins.java
+++ src/main/java/icu/mhb/mybatisplus/plugln/extend/Joins.java
@@ -1,4 +1,5 @@
 package icu.mhb.mybatisplus.plugln.extend;
+
 import icu.mhb.mybatisplus.plugln.core.JoinLambdaWrapper;
 
 /**
@@ -11,6 +12,10 @@ public class Joins {
 
     public static <T> JoinLambdaWrapper<T> of(Class<T> clz) {
         return new JoinLambdaWrapper<>(clz);
+    }
+
+    public static <T> JoinLambdaWrapper<T> of(Class<T> clz, String alias) {
+        return new JoinLambdaWrapper<>(clz, alias);
     }
 
 }
