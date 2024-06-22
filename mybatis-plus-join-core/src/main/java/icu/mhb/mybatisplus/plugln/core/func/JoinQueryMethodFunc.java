7b483975733e28d7cc8128fb57380da678b8c984
马汇博 <mhb0409@qq.com>
2024-06-23 00:52:32 +0800

    2.0.0 第二次版本初始化

---
+++ mybatis-plus-join-core/src/main/java/icu/mhb/mybatisplus/plugln/core/func/JoinQueryMethodFunc.java
@@ -0,0 +1,13 @@
+package icu.mhb.mybatisplus.plugln.core.func;
+
+/**
+ * @author mahuibo
+ * @Title: JoinQueryMethodFunc
+ * @email mhb0409@qq.com
+ * @time 2024/6/22
+ */
+public interface JoinQueryMethodFunc<Children> {
+
+
+
+}
