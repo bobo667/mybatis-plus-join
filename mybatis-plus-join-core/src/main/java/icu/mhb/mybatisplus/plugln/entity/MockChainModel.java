f1e30cd46e7271d78559a12ce2b8949ca2a197d9
马汇博 <mhb0409@qq.com>
2024-06-23 00:52:54 +0800

    2.0.0 第二次版本初始化

---
+++ mybatis-plus-join-core/src/main/java/icu/mhb/mybatisplus/plugln/entity/MockChainModel.java
@@ -0,0 +1,15 @@
+package icu.mhb.mybatisplus.plugln.entity;
+
+/**
+ * @author mahuibo
+ * @Title: MockChainModel
+ * @email mhb0409@qq.com
+ * @time 2024/6/22
+ */
+public class MockChainModel extends BaseChainModel<MockChainModel> {
+
+    public MockChainModel(String alias, Class<?> modelClass) {
+        super(alias, modelClass);
+    }
+
+}
