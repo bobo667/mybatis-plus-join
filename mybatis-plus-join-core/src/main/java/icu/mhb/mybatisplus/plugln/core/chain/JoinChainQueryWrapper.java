11a60b921565d5d5aa935553df1331b8193233bb
马汇博 <mhb0409@qq.com>
2024-06-23 00:51:51 +0800

    2.0.0 第二次版本初始化

--- mybatis-plus-join-core/src/main/java/icu/mhb/mybatisplus/plugln/core/chain/JoinChainQueryWrapper.java
+++ mybatis-plus-join-core/src/main/java/icu/mhb/mybatisplus/plugln/core/chain/JoinChainQueryWrapper.java
@@ -1,31 +1,26 @@
 package icu.mhb.mybatisplus.plugln.core.chain;
 
 import com.baomidou.mybatisplus.core.conditions.SharedString;
-import com.baomidou.mybatisplus.core.conditions.query.Query;
 import com.baomidou.mybatisplus.core.conditions.segments.MergeSegments;
 import com.baomidou.mybatisplus.core.metadata.TableFieldInfo;
 import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
-import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
-import icu.mhb.mybatisplus.plugln.base.mapper.JoinBaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import icu.mhb.mybatisplus.plugln.constant.StringPool;
 import icu.mhb.mybatisplus.plugln.core.JoinLambdaWrapper;
-import icu.mhb.mybatisplus.plugln.core.chain.func.JoinChainCompareFun;
 import icu.mhb.mybatisplus.plugln.core.chain.support.SupportJoinChainQueryWrapper;
-import icu.mhb.mybatisplus.plugln.core.func.JoinMethodFunc;
 import icu.mhb.mybatisplus.plugln.core.func.JoinQueryFunc;
-import icu.mhb.mybatisplus.plugln.core.support.SupportJoinLambdaWrapper;
 import icu.mhb.mybatisplus.plugln.entity.*;
+import icu.mhb.mybatisplus.plugln.exception.Exceptions;
 import icu.mhb.mybatisplus.plugln.keyword.IFuncKeyWord;
+import icu.mhb.mybatisplus.plugln.tookit.Provider;
 import lombok.Getter;
 
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-
-import static com.baomidou.mybatisplus.core.toolkit.StringPool.COMMA;
 
 /**
  * @author mahuibo
@@ -35,37 +30,8 @@ import static com.baomidou.mybatisplus.core.toolkit.StringPool.COMMA;
  */
 @SuppressWarnings("all")
 public class JoinChainQueryWrapper<T> extends SupportJoinChainQueryWrapper<T, JoinChainQueryWrapper<T>>
-        implements JoinQueryFunc<T, JoinLambdaWrapper<T>> {
+        implements JoinQueryFunc<T, String, JoinChainQueryWrapper<T>> {
 
-
-    /**
-     * 主表别名
-     */
-    @Getter
-    private String masterTableAlias;
-
-    /**
-     * 主表class
-     */
-    private Class<T> masterClass;
-
-    /**
-     * 关键字获取
-     */
-    private IFuncKeyWord funcKeyWord;
-
-
-    /**
-     * 一对一 构建列表
-     */
-    @Getter
-    private List<OneToOneSelectBuild> oneToOneSelectBuildList = null;
-
-    /**
-     * 多对多 构建列表
-     */
-    @Getter
-    private List<ManyToManySelectBuild> manyToManySelectBuildList = null;
 
     /**
      * 判断SQL是否缓存过
@@ -89,53 +55,28 @@ public class JoinChainQueryWrapper<T> extends SupportJoinChainQueryWrapper<T, Jo
     private SharedString sqlSelectCahce = new SharedString();
 
 
+    @Override
+    public String getSqlSelect() {
 
-//
-//    public String getSqlSelect() {
-//
-//        if (sqlSelectFlag) {
+        if (sqlSelectFlag) {
+            return sqlSelectCahce.getStringValue();
+        }
 
-//            return sqlSelectCahce.getStringValue();
-//        }
-//
-//        if (CollectionUtils.isEmpty(sqlSelect) && !this.notDefaultSelectAll) {
-//            selectAll();
-//        }
-//
-//        StringBuilder stringValue = new StringBuilder(sqlSelect.stream().map(SharedString::getStringValue).distinct().collect(Collectors.joining(",")));
-//
-//        String joinSelectSql = joinSqlSelect.stream()
-//                .map(SharedString::getStringValue)
-//                .filter(StringUtils::isNotBlank)
-//                .collect(Collectors.joining(COMMA));
-//
-//        // 只有在拥有主表查询字段并且有子表查询的时候才需要加上','分隔符
-//        if (stringValue.length() > 0 && StringUtils.isNotBlank(joinSelectSql)) {
-//            stringValue.append(COMMA);
-//        }
-//
-//        stringValue.append(joinSelectSql);
-//
-//        if (CollectionUtils.isNotEmpty(sunQueryList)) {
-//            // 只有在拥有主表查询字段并且有子表查询的时候才需要加上','分隔符
-//            if (stringValue.length() > 0) {
-//                stringValue.append(COMMA);
-//            }
-//            stringValue.append(sunQueryList.stream().map(SharedString::getStringValue).collect(Collectors.joining(",")));
-//        }
-//
-//        String selectSql = stringValue.toString();
-//
-//        sqlSelectFlag = true;
-//        if (hasDistinct) {
-//            selectSql = getFuncKeyWord().distinct() + " " + selectSql;
-//        }
-//        sqlSelectCahce.setStringValue(selectSql);
-//
-//        return selectSql;
-//    }
+        if (CollectionUtils.isEmpty(sqlSelect) && !this.notDefaultSelectAll) {
+            selectAll(masterChainModel);
+        }
 
+        StringBuilder stringValue = new StringBuilder(sqlSelect.stream().map(SharedString::getStringValue).distinct().collect(Collectors.joining(",")));
 
+        sqlSelectFlag = true;
+        if (hasDistinct) {
+            stringValue.insert(0, getFuncKeyWord().distinct() + StringPool.SPACE);
+        }
+        String selectSql = stringValue.toString();
+
+        sqlSelectCahce.setStringValue(selectSql);
+        return selectSql;
+    }
 
 
     @Override
@@ -147,11 +88,8 @@ public class JoinChainQueryWrapper<T> extends SupportJoinChainQueryWrapper<T, Jo
 
     public JoinChainQueryWrapper(BaseChainModel model) {
         super.setEntityClass(model.getModelClass());
-        super.initNeed();
-    }
-
-    public JoinChainQueryWrapper(Class<T> cla) {
-        super.setEntityClass(cla);
+        this.masterChainModel = model;
+        this.masterTableAlias = model.getAlias();
         super.initNeed();
     }
 
@@ -159,6 +97,7 @@ public class JoinChainQueryWrapper<T> extends SupportJoinChainQueryWrapper<T, Jo
     JoinChainQueryWrapper(T entity, Class<T> entityClass, List<SharedString> sqlSelect, AtomicInteger paramNameSeq,
                           Map<String, Object> paramNameValuePairs, MergeSegments mergeSegments,
                           SharedString lastSql, SharedString sqlComment, SharedString sqlFirst) {
+        initNeed();
         super.setEntity(entity);
         super.setEntityClass(entityClass);
         this.paramNameSeq = paramNameSeq;
@@ -171,13 +110,17 @@ public class JoinChainQueryWrapper<T> extends SupportJoinChainQueryWrapper<T, Jo
     }
 
     @Override
-    public JoinChainQueryWrapper<T> select(String... columns) {
-        return null;
+    public JoinChainQueryWrapper<T> select(Class<T> entityClass, Predicate<TableFieldInfo> predicate) {
+        throw Exceptions.mpje("请不要调用该方法！请使用 select(BaseChainModel<?> model, Predicate<TableFieldInfo> predicate)");
     }
 
     @Override
-    public JoinChainQueryWrapper<T> select(Class<T> entityClass, Predicate<TableFieldInfo> predicate) {
-        return null;
+    public String getJoinSql() {
+        Collection<SharedString> joinSqlList = joinSqlMapping.values();
+        if (CollectionUtils.isNotEmpty(joinSqlList)) {
+            return joinSqlList.stream().map(SharedString::getStringValue).collect(Collectors.joining(Constants.NEWLINE));
+        }
+        return "";
     }
 
 }
